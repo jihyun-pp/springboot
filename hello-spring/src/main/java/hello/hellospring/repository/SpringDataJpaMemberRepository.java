19cd9523aab04ea328c6b6bc35c61f904cec323e
jihyun-pp <qkrwlw1211@gmail.com>
2023-02-14 17:24:50 +0900

    스프링데이터JPA

--- hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -1,2 +1,14 @@
-package hello.hellospring.repository;public interface SpringDataJpaMemberRepository {
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+    // 스프링 데이터 JPA가 proxy를 이용해서 SpringDataJpaMemberRepository를 자동으로 스프링 빈 객체를 생성해 줌
+
+    @Override
+    Optional<Member> findByName(String name);
+    // JPQL > select m from Member m where m.name = ?
 }
