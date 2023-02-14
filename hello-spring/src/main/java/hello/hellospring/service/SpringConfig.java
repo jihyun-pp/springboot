f3e40883ed66159ed400212f5c00d42b0fdbfbf8
jihyun-pp <qkrwlw1211@gmail.com>
2023-02-14 17:51:43 +0900

    aop

--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring.service;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
@@ -10,13 +11,13 @@ import org.springframework.context.annotation.Configuration;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-@Configuration
+//@Configuration
 public class SpringConfig {
     // 컴포넌트 대신에 사용하는 스프링빈 객체
 
     private EntityManager em;
 
-    @Autowired
+//    @Autowired
     public SpringConfig(EntityManager em){
         this.em = em;
     }
@@ -30,5 +31,10 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
         return new JpaMemberRepository(em);
+    }
+
+    @Bean
+    public TimeTraceAop timeTraceAop() {
+        return new TimeTraceAop();
     }
 }
