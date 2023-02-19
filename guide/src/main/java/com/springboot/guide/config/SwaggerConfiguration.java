7f77c39327e40eb4cdafba872b135381ba09f814
jihyun-pp <qkrwlw1211@gmail.com>
2023-02-19 23:53:35 +0900

    swagger 빈 객체

--- guide/src/main/java/com/springboot/guide/config/SwaggerConfiguration.java
+++ guide/src/main/java/com/springboot/guide/config/SwaggerConfiguration.java
@@ -1,2 +1,33 @@
-package com.springboot.guide.config;public class SwaggerConfiguration {
+package com.springboot.guide.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    /**
+     * REST API 명세 문서화 : Swagger
+     */
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot Open API Test with Swagger")
+                .description("설명 부분")
+                .version("1.0.0")
+                .build();
+    }
 }
