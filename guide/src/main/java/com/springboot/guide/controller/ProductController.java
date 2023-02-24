7e94f1e7a2fa973eae7020c3366fc0f05ec50129
jihyun-pp <qkrwlw1211@gmail.com>
2023-02-24 16:08:27 +0900

    product controller

---
+++ guide/src/main/java/com/springboot/guide/controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.springboot.guide.controller;
+
+import com.springboot.guide.data.dto.ChangeProductDto;
+import com.springboot.guide.data.dto.ProductDto;
+import com.springboot.guide.data.dto.ProductResponseDto;
+import com.springboot.guide.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<ProductResponseDto> getProduct(Long number){
+        ProductResponseDto productResponseDto = productService.getProduct(number);
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @PostMapping()
+    public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductDto productDto){
+        ProductResponseDto productResponseDto = productService.saveProduct(productDto);
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @PutMapping()
+    public ResponseEntity<ProductResponseDto> changeProductName(@RequestBody ChangeProductDto changeProductDto) throws Exception{
+        ProductResponseDto productResponseDto = productService.changeProductName(changeProductDto.getNumber(), changeProductDto.getName());
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<String> deleteProduct(Long number) throws Exception {
+        productService.deleteProduct(number);
+        return ResponseEntity.status(HttpStatus.OK).body("정상적으로 삭제되었습니다.");
+    }
+}
