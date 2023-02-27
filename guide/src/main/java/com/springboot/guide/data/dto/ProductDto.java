1ec1acf486fa1d5f65d4567f4ad1485e7fc0e9c1
jihyun-pp <qkrwlw1211@gmail.com>
2023-02-28 00:07:21 +0900

    lombok

--- guide/src/main/java/com/springboot/guide/data/dto/ProductDto.java
+++ guide/src/main/java/com/springboot/guide/data/dto/ProductDto.java
@@ -1,8 +1,14 @@
 package com.springboot.guide.data.dto;
 
-import lombok.Data;
+import lombok.*;
 
-@Data
+import javax.persistence.Entity;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductDto {
     private String name;
     private int price;
