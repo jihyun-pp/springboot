ac61653fca73a3ad6b74cf060abe24b925f83e80
jihyun-pp <qkrwlw1211@gmail.com>
2023-03-04 19:37:16 +0900

    lombok

--- guide/src/main/java/com/springboot/guide/data/entity/Product.java
+++ guide/src/main/java/com/springboot/guide/data/entity/Product.java
@@ -5,11 +5,11 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name="product")
 public class Product {
 
