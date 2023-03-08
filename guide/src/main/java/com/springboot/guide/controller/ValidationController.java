819f342069a63afc0b8413d4e34795025f67ae64
jihyun-pp <qkrwlw1211@gmail.com>
2023-03-08 21:12:23 +0900

    validation

---
+++ guide/src/main/java/com/springboot/guide/controller/ValidationController.java
@@ -0,0 +1,28 @@
+package com.springboot.guide.controller;
+
+import com.springboot.guide.dto.ValidRequestDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/validation")
+public class ValidationController {
+
+    private final Logger logger = LoggerFactory.getLogger(ValidationController.class);
+
+    @PostMapping("/valid")
+    public ResponseEntity<String> checkValidationByValid(@Valid @RequestBody ValidRequestDto validRequestDto){
+        logger.info(validRequestDto.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(validRequestDto.toString());
+    }
+
+
+}
